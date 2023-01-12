f8feac320008f23e00a8fa2c7bc67aede41e6a66
= <=>
2023-01-12 12:24:27 -0400

    Se creó la lista de criptos, se agrego Axios y se creó el archivo CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
 const App = () => {
+
+  const [criptos, setCriptos] = useState()
+
+  const API_URL = import.meta.env.VITE_APP_URL
+
+  useEffect(() => {
+    axios.get(`${API_URL}assets`)
+      .then(data => {
+        setCriptos(data.data.data)
+      })
+      .catch(err => console.error(err))
+  }, [])
+
+  if(!criptos) return <span>Cargando...</span>
+
   return (
-    <div>Hola mundo!</div>
+    <>
+      <h1>Lista de criptomonedas</h1>
+      <ol>
+          {
+            criptos.map(({id, name, priceUsd}) => (
+              <li key={id}>Nombre: {name} Precio: {priceUsd}</li>
+            ))}
+      </ol>    
+    </>
   )
 }
+
 export default App;
